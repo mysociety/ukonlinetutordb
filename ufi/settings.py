a70ca8ecd7e6a408fd512dc5d75e31b2deb7ee9f
Edmund von der Burg <evdb@mysociety.org>
2011-04-19 12:40:45 +0100

    Setup project to run under mySoc infrastructure

--- ufi/settings.py
+++ ufi/settings.py
@@ -1,20 +1,52 @@
 # Django settings for ufi project.
 
-DEBUG = True
-TEMPLATE_DEBUG = DEBUG
+# Some special mysociety preamble in order to get hold of our config
+# file conf/general
+import os
+import sys
+package_dir = os.path.abspath(os.path.split(__file__)[0])
 
+paths = (
+    os.path.normpath(package_dir + "/../pylib"),
+    os.path.normpath(package_dir + "/../commonlib/pylib"),
+    )
+
+for path in paths:
+    if path not in sys.path:
+        sys.path.append(path)
+
+try:
+    from config_local import config  # put settings in config_local if you're not running in a fill mysociety vhost
+    SERVE_STATIC_FILES = True
+except ImportError:
+    SERVE_STATIC_FILES = False
+    from mysociety import config
+    config.set_file(os.path.abspath(package_dir + "/../conf/general"))
+
+# Now follows the normal Django stuff.
+
+if int(config.get('STAGING')):
+    DEBUG = True
+else:
+    DEBUG = False
+TEMPLATE_DEBUG = DEBUG
 ADMINS = (
     # ('Your Name', 'your_email@domain.com'),
 )
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = ''           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = ''             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
+DATABASE_ENGINE   = 'postgresql_psycopg2'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+DATABASE_NAME     = config.get('UFI_DB_NAME')
+DATABASE_USER     = config.get('UFI_DB_USER')
+DATABASE_PASSWORD = config.get('UFI_DB_PASS')
+DATABASE_HOST     = config.get('UFI_DB_HOST')
+DATABASE_PORT     = config.get('UFI_DB_PORT')
+
+MAPIT_URL = config.get('MAPIT_URL')
+
+# a postGIS template, e.g.: http://docs.djangoproject.com/en/dev/ref/contrib/gis/install/#creating-a-spatial-database-template-for-postgis
+POSTGIS_TEMPLATE = 'template_postgis'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
@@ -25,7 +57,7 @@ TIME_ZONE = 'Europe/London'
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
-LANGUAGE_CODE = 'en-gb'
+LANGUAGE_CODE = 'en-GB'
 
 SITE_ID = 1
 
@@ -35,7 +67,7 @@ USE_I18N = True
 
 # Absolute path to the directory that holds media.
 # Example: "/home/media/media.lawrence.com/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(package_dir, "../web/static/")
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
@@ -48,7 +80,7 @@ MEDIA_URL = ''
 ADMIN_MEDIA_PREFIX = '/media/'
 
 # Make this unique, and don't share it with anybody.
-SECRET_KEY = '(wxf5dg*4bf)mowv68co6_=l@8yav+--z#@v17=#tz$(jaom=s'
+SECRET_KEY = config.get('DJANGO_SECRET_KEY')
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
@@ -69,6 +101,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(package_dir, "templates"),
 )
 
 INSTALLED_APPS = (
