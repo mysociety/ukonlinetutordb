7b78737871d10b68c8c4ce4eba50ae8c7c978124
Edmund von der Burg <evdb@mysociety.org>
2011-05-17 13:10:00 +0100

    Show template previews on the create page

--- pylib/ukotdb-app/certificates/forms.py
+++ pylib/ukotdb-app/certificates/forms.py
@@ -20,6 +20,10 @@ class CertificateForm(forms.ModelForm):
         centre_qs = Centre.objects.filter( id__in=centre_ids)
         self.fields['centre'] = forms.ModelChoiceField(queryset=centre_qs, empty_label=None)
 
+        # limit templates to those that are active
+        # TODO - code this
+        # self.fields['template'] = forms.ModelChoiceField(queryset=...., empty_label=None)
+
         # set some initial values
         self.fields['tutor_name'].initial   = user.get_full_name()
         self.fields['date_awarded'].initial = datetime.now
