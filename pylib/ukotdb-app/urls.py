f4de9d6fbccb29143b71a3e07972098de1f2d068
Edmund von der Burg <evdb@mysociety.org>
2011-05-19 11:24:47 +0100

    Allow user to reset their passwords (using default templates)

--- pylib/ukotdb-app/urls.py
+++ pylib/ukotdb-app/urls.py
@@ -6,11 +6,15 @@ import settings
 # find all the admin setup
 admin.autodiscover()
 
-urlpatterns = patterns('',
+# accounts
+urlpatterns = patterns('django.contrib.auth.views',
+    ( r'^accounts/login/$',  'login'          ),
+    ( r'^accounts/logout/$', 'logout'         ),
 
-    # accounts
-    (r'^accounts/login/$',  'django.contrib.auth.views.login'),
-    (r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
+    ( r'^accounts/reset/$',          'password_reset'          ),
+    ( r'^accounts/reset_done/$',     'password_reset_done'     ),
+    ( r'^accounts/reset_confirm/(?P<uidb36>\d+)/(?P<token>[\d\w-]+)$','password_reset_confirm'  ),
+    ( r'^accounts/reset_complete/$', 'password_reset_complete' ),
 )
 
 urlpatterns += patterns('tutordb.views.tutor',
@@ -21,10 +25,10 @@ urlpatterns += patterns('tutordb.views.tutor',
     ( r'^tutors/',          'tutor_list'   ),
 )
 
+# centres
 urlpatterns += patterns('tutordb.views.centre',
-    # centres
-    (r'^centres/$',                    'centre_list'   ),
-    (r'^centres/(?P<centre_id>\d+)/$', 'centre_detail' ),
+    ( r'^centres/$',                    'centre_list'   ),
+    ( r'^centres/(?P<centre_id>\d+)/$', 'centre_detail' ),
 )
 
 
