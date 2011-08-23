4dde1847b9416da3f6ac614eb05dbc4e0c7fc8a3
Struan Donald <struan@exo.org.uk>
2011-08-23 15:07:45 +0100

    add in tutor name to certificate list display

--- pylib/ukotdb-app/certificates/admin.py
+++ pylib/ukotdb-app/certificates/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from certificates import models
+from certificates.models import Certificate, CertificateTemplate
 
-admin.site.register(models.Certificate)
-admin.site.register(models.CertificateTemplate)
+class CertificateAdmin(admin.ModelAdmin):
+    list_display = ( 'student_name', 'course_name', 'tutor_name', )
+
+admin.site.register(Certificate, CertificateAdmin)
+admin.site.register(CertificateTemplate)
